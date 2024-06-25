574cee3caea4beee6ac3ec535dd89068de5d41fd
toeten <n.santiago0228@gmail.com>
2024-06-25 13:45:33 -0400

    initializing branch

--- hounds-project/src/main.js
+++ hounds-project/src/main.js
@@ -1,14 +1,17 @@
 import { fetchRandomDog } from './fetchFunctions.js';
 import { renderDog } from './renderFunctions.js';
 
+import { fetchSingularJoke } from './fetchFunctions.js';
+import { renderSingularJoke } from './renderFunctions.js';
 
-
-
-const main = () => {
+const main = async () => {
   document.getElementById('fetch-dog').addEventListener('click', async () => {
     const dog = await fetchRandomDog();
     if (dog) renderDog(dog);
-  })
+  });
+
+  const joke = await fetchSingularJoke();
+  renderSingularJoke(joke);
 }
 
 main()
